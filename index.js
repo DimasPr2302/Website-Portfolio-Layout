5d50b605b444b322b9a7a9070232c198d71bb716
Dimas_Prasetyo <106918201+DimasPr2302@users.noreply.github.com>
2023-03-01 09:51:09 +0700

    Menambahkan file HTML, CSS, JS
    This is submission for dicoding course.

---
+++ index.js
@@ -0,0 +1,8 @@
+
+let hamburger = document.querySelector(".toggle-icon");
+hamburger.onclick = function () {
+    navBar = document.querySelector(".nav-list");
+    navBar.classList.toggle("active");
+}
+
+alert("Hai! Welcome to my website")
